5c30cbf777887358e0c58f97d77ce89bb38f4c63
Jose Alberto Gomez <josequesada9393@gmail.com>
2023-01-15 22:07:13 -0800

    reviews bg

--- client/no-waste/src/Components/ReviewsPortal/ReviewsItems.js
+++ client/no-waste/src/Components/ReviewsPortal/ReviewsItems.js
@@ -5,9 +5,9 @@ function ReviewsItems({review}) {
     <div className='mainReviewsContainer'>
       <div className='ReviewsContainer'>
 <article class="center br3 mw5 mw6-ns hidden ba mv4 ">
-          <h1 class="f4 br3 bg-near-black white mv0 pv2 ph3">by {review.userThatPostsReviewName}</h1>
+          <h1 class="f4 br3\4 bg-near-black white mv0 pv2 ph3">by {review.userThatPostsReviewName}</h1>
   <div class="pa3 bt">
-            <p class="f6 f5-ns lh-copy measure mv0">
+            <p class="f6 f5-ns lh-copy bg-white measure mv0 o-50">
               {review.review}
     </p>
   </div>
