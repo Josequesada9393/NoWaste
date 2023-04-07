4582d6a4df2348af30e53a3873e0edd628089b23
Jose Alberto Gomez <josequesada9393@gmail.com>
2023-04-07 14:48:18 -0700

    fixed google maps api bug

--- client/no-waste/src/Components/MAP/Map.js
+++ client/no-waste/src/Components/MAP/Map.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import { GoogleMap, MarkerF, useLoadScript, InfoWindow } from "@react-google-maps/api";
+import { GoogleMap, MarkerF, useLoadScript, InfoWindow, useJsApiLoader } from "@react-google-maps/api";
 import MarkerItem from './MarkerItem';
 import homeIcon from './place.png'
 import { useState } from 'react';
 
 function Map({itemsShared}) {
 
-  const {isLoaded } = useLoadScript({
-    googleMapsApiKey: "AIzaSyBQWlb7R2WqmHa_UqFegIPdyFXs3hbYz3M"
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
   })
 
   const home = { lat: 33.6189, lng: -117.9298 };
@@ -32,7 +33,7 @@ const divStyle = {
           >
           <img className="center bg-white tc" style={{width: '50px', height: '50px', marginRight: "11px"}} src="https://static.vecteezy.com/system/resources/previews/004/416/880/original/simple-house-icon-on-white-background-free-vector.jpg"></img>
           </InfoWindow>
-        }
+
           </MarkerF>
 
             {
