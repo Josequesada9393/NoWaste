096f227dd5d33aa3d09c0b2c8bb58e1fb4706d1a
Jose Alberto Gomez <josequesada9393@gmail.com>
2023-06-03 09:51:56 -0700

    vercel 3

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const cors = require('cors');
-
 const express = require('express');
 const app = express();
-app.use(cors())
+app.use(cors({
+  origin: '*'
+}))
 
 const router = require('./router.js');
 const PORT = 4000;
